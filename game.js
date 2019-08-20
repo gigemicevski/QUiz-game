ba68fb07d232d94bb60521742aa28c3199bf8c0d
gigemicevski <gigemicevski@gmail.com>
2019-08-20 18:51:51 +0200

    score and questions

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,6 +54,9 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
+  //question counter
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
   const questionIndex = Math.floor(Math.random() * availableQuesions.length);
   currentQuestion = availableQuesions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -77,14 +82,25 @@ choices.forEach(choice => {
 
     const classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+      
+      if (classToApply === "correct") {
+        incrementScore(CORRECT_BONUS);
+      }
 
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
-    }, 1000);
+    }, 200);
   });
 });
+
+//rezultat
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
+
 
 startGame();
